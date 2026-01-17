64952e9c4bfee27afd729b739c08c17a4bdcf01d
Adithya Ganesan <adithyaganesan@outlook.com>
2026-01-16 19:45:56 -0700

    Initialize sudoku board and find candidates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 mod board;
+use board::SudokuBoard;
 
 fn main() {
-    println!("Hello, world!");
+    let puzzle = "530070000600195000098000060800060003400803001700020006060000280000419005000080079";
+
+    match SudokuBoard::new(puzzle) {
+        Some(board) => {
+            println!("Board initialized successfully");
+            let candidates = board.get_candidates(0, 2);
+            println!("Candidates for cell (0, 2) in bitmask: {:b}", candidates);
+
+            for val in 1..=9 {
+                if candidates & (1 << val) != 0 {
+                    println!("{}", val);
+                }
+            }
+        }
+        None => {
+            println!("Failed to initialize board");
+        }
+    }
 }
